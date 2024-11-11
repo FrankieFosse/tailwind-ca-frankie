7a5a7c1f5a4532bcd6c3d6dc26317544ec181804
FrankieFosse <146066047+FrankieFosse@users.noreply.github.com>
2024-11-11 20:18:28 +0100

    responsiveness241111

--- dist/feed/details.js
+++ dist/feed/details.js
@@ -28,11 +28,11 @@ getPostById();
 
 function listDetailedPost(post, out) {
     let newDiv = `
-    <div class="text-gray-100 bg-gray-900 flex flex-col h-4/5 lg:w-3/5 xl:w-2/5 w-4/5 text-center items-center justify-center content-center mt-16 overflow-hidden">
+    <div class="text-gray-100 bg-gray-900 flex flex-col h-4/5 lg:w-2/5 xl:w-2/5 w-4/5 text-center items-center justify-center content-center mt-5 overflow-hidden">
     <p class="w-4/5 mt-5">Made by <a class="bg-blue-600 saturate-50 rounded-full px-2 hover:bg-blue-400 duration-150" href="/dist/profile/index.html?name=${post.data.author.name}">${post.data.author.name}</a></p>
     <h1 class="h-4/5 w-4/5 mb-3 mt-10 mx-10">${post.data.title}</h1>
     <img src=${post.data.media?.url ? post.data.media?.url : "/../images/image-placeholder-500x500.jpg"} class="w-2/5">
-    <p class="h-4/5 w-auto pb-16 pt-4 mx-10">${post.data.body}</p>
+    <p class="h-4/5 w-auto pb-5 pt-4 mx-10">${post.data.body}</p>
     </div>
     `;
     out.innerHTML = newDiv;
